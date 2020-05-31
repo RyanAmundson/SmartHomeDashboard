841fd1d9c710908ff19cdea05f5b448ed36211ef
Ryan Amundson <ryanjamundson@gmail.com>
2020-05-31 13:57:05 -0700

    add route back in

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from "@angular/core";
 import { Routes, RouterModule } from "@angular/router";
 import { PageNotFoundComponent } from './_common/page-not-found/page-not-found.component';
 import { AppComponent } from './app.component';
+import { DashboardModule } from './dashboard/dashboard.module';
 
 
 
@@ -11,6 +12,10 @@ const routes: Routes = [
   {
     path: "mobile",
     loadChildren: () => import('./_mobile/mobile.module').then(m => m.MobileModule),
+  },
+  {
+    path: "dashboard",
+    loadChildren: () => import('./_dashboard/dashboard.module').then(m => m.DashboardModule),
   },
   { path: '', redirectTo: 'mobile', pathMatch: 'full' },
   { path: '**', component: PageNotFoundComponent },
